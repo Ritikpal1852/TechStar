b3948942d8bbb63408069b79f0b301672f83a868
ritikpal1852 <18520247>
2022-12-15 01:07:12 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+      
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+        <a href="https://www.linkedin.com/in/ritik-pal-7a4693216/" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/ritikpal1614/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Ritikpal1852" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+      <div>
+        <h3>About Creater</h3>
+        <p>Ritik Pal (Designer/Coder)</p>
+        <p>M.Sc Chemistry (IIT Hyderabad)</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
